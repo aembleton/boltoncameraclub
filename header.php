ae837d6ed36e8696bb9cee737b54cbd943faf366
Arthur Embleton <arthurembleton@rideways.com>
2019-03-18 18:06:37 +0000

    Created a basic first page

---
+++ header.php
@@ -0,0 +1,43 @@
+<?
+    $about_us_active = "";
+    $syllabus_active = "";
+    $competition_results_active = "";
+    switch($page) {
+        case "about-us" : $about_us_active = "active"; break;
+        case "syllabus" : $syllabus_active = "active"; break;
+        case "competition-results" : $competition_results_active = "active"; break;
+    }
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title><? echo $title ?></title>
+    </head>
+    <body class="bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">
+    <img src="http://www.boltoncameraclub.co.uk/templates/shape5_vertex/images/s5_logo.png">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item <? echo $about_us_active ?>">
+        <a class="nav-link" href="#">About Us</a>
+      </li>
+      <li class="nav-item <? echo $syllabus_active ?>">
+        <a class="nav-link" href="#">Syllabus</a>
+      </li>
+      <li class="nav-item <? echo $competition_results_active ?>">
+        <a class="nav-link" href="#">Competition Results</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container bg-white shadow-lg mt-5 p-3">
---
+++ index.php
@@ -0,0 +1,16 @@
+<? 
+$title = "Welcome to Bolton Camera Club";
+$page = "about-us";
+include('header.php'); ?>
+    <h1><? echo $title ?></h1>
+    <p>Bolton Camera Club meets weekly throughout most of the year and gives a very warm welcome to visitors and new members. We have been sharing a passion for photography since 1884 and are more than happy to share with you, whether your interest is in film or digital, audio visual or more technical aspects of photography. </p>
+    <h2>Where we meet</h2>
+    <p>We meet at 7:15 PM on Thursday evenings at <a href="https://goo.gl/maps/q47R5iYDDRA2">St Osmund's Church Hall, Long Lane, Bolton.  BL2 6EB</a>.  There is a large car park at the church.</p>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d18950.604921709204!2d-2.404110276538087!3d53.578728796203755!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487ba663251c12bb%3A0x4851b47e5df84265!2sSt+Osmunds+R+C+Church!5e0!3m2!1sen!2suk!4v1552931107886" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <h3>Coming from Bolton Town Centre</h3>
+    <p>Follow Bury New Road or Bradford Street onto Bury Road. Turn right at the second set of lights on to Long Lane. St Osmund's Church Hall is then on the left hand side</p>
+    <h3>Coming from North Bolton</h3>
+    <p>Join the ring road and travel clockwise along Crompton Way. At the end of Crompton Way, turn left  onto Bury Road. At the next set of traffic lights turn right, onto Long Lane. St Osmund's Church Hall is then on the left hand side</p>
+    <h3>Coming from Bury</h3>
+    <p>Follow Bury Road until the shopping centre at Breightmet is on your right. At the next traffic lights turn left onto Long Lane. St Osmund's Church Hall is just on the left hand side</p>
+<? include('footer.php'); ?>
