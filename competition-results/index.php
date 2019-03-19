46f564e5b3fd6e91411519dd87501e40ff73a4a5
Arthur Embleton <arthurembleton@rideways.com>
2019-03-19 14:07:54 +0000

    Loading the shared google doc

--- competition-results/index.php
+++ competition-results/index.php
@@ -4,7 +4,7 @@ $page = "competition-results";
 include('../inc/header.php');
 
 $dom = new DOMDocument();
-$dom->loadHTMLFile('https://docs.google.com/document/d/e/2PACX-1vQ--t3WdjMkTyTZ_M8QSIRuIA1gua-29gQlQ0lpSbjkGB4F_RTv41626ZM-eWYgp14O7BZNapE4u7V9/pub?embedded=true');
+$dom->loadHTMLFile('https://docs.google.com/document/d/e/2PACX-1vQeSp58XPhA_CCHRxjFvKi9x5jE4-VH_7ismztu49KceE38cEPGo67gKzYqqrjxRpSVU--DkGFgbPCu/pub?embedded=true');
 echo $dom->saveHTML();
 
  include('../inc/footer.php'); 
